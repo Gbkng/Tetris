77cf0818ab8088990afacbcfaa584d7ed20944d9
Gbkng <guillaume.brooking@gmail.com>
2021-12-18 22:54:00 +0100

    wip

---
+++ src/random_generator.hpp
@@ -0,0 +1,26 @@
+#ifndef GUYTETRIS_UTILS_HPP_
+#define GUYTETRIS_UTILS_HPP_
+
+#include "tetromino.hpp"
+#include <random>
+
+namespace tetris {
+
+class RandomGenerator {
+public:
+  RandomGenerator() {
+    std::random_device dev;
+    std::mt19937 rng_(dev());
+    std::uniform_int_distribution<std::mt19937::result_type>
+        random_distribution_(1, Tetromino::shapeNb);
+  }
+  int generateRandomInteger() { return random_distribution_(rng_); }
+
+private:
+  std::mt19937 rng_;
+  std::uniform_int_distribution<std::mt19937::result_type>
+      random_distribution_; // distribution in range [1, shapeNb]
+};
+} // namespace tetris
+
+#endif
