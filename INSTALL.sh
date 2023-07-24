de98253d426fd55115ca9359c3d3c9fc584f00d2
Gbkng <guillaume.brooking@gmail.com>
2023-07-24 15:30:23 +0200

    update install cript

--- INSTALL.sh
+++ INSTALL.sh
@@ -3,26 +3,39 @@
 echo -e "\
 ---------------------
 Tetris install script
----------------------"
+---------------------
+"
 
-echo "install dependencies"
+if ! [ -f '/etc/debian_version' ]; then
+    echo "It seems that your distribution is not debian-based. The install \
+procedure may fail."
+fi
 
-sudo apt install -y \
-  gcc \
-  libstdc++6  \
-  make  \
-  cmake  \
-  libsfml-dev  \
-  libsfml-graphics2.5  \
-  libsfml-system2.5  \
-  libsfml-window2.5
+echo "Please run the following command to ensure dependencies are correctly \
+installed:
 
-echo "configure project"
+'sudo apt install -y \
+gcc \
+libstdc++6 \
+make \
+cmake \
+libsfml-dev \
+libsfml-graphics2.5 \
+libsfml-system2.5 \
+libsfml-window2.5'\
+"
+
+printf  "\nconfigure project with 'cmake'\n\n"
+
+set -e
 
 mkdir -p bin
+
 cmake -S . -B bin -DCMAKE_BUILD_TYPE=Release
 
-echo "build"
+printf  "\n\nbuild project with 'make'\n\n"
 
 make -C bin
 chmod +x bin/tetris
+
+set +e
