ad61a14e3654bb9f63e72ee59d8a825bd23d40e0
Gbkng <guillaume.brooking@gmail.com>
2023-07-24 14:52:05 +0200

    fix includes and format

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,8 @@
-#include <iostream>
-
-#include "SFML/Graphics.hpp"
 #include "game.hpp"
-#include "game_display.hpp"
-#include "tetromino.hpp"
 
-int main()
-{
+int main() {
   tetris::TetrisGame<10, 20> game;
   game.run();
+
   return 0;
 }
